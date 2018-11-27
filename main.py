c2e418bd42ec22616e49febeba4e29acc7d20e60
Eddy Reyes <ereyes01@users.noreply.github.com>
2018-11-27 01:41:43 -0500

    Add mindsight sampler to induce sending data to the agent

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from test import main_module
+import sampler
 
 
 if __name__ == "__main__":
+    sampler.start(server_url="http://localhost:8000",project="news_reader",modules=["test"], send_after=10)
+
     how_many = input("How hard shall I work? (Enter a number): ")
 
     main_module.spin(how_many)
